482c43408b8875c26dc16ba03b80eac7a435d7d5
Fernando <fernando@velocity.black>
2020-05-02 23:47:28 +0300

    Add coloured progress indicator and click to reset

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -2,35 +2,46 @@ import React, { useState } from 'react'
 import styles from './Timer.module.scss'
 import { splitSeconds } from '../../utils/parse'
 import useInterval from '../../utils/useInterval'
-import clsx from 'clsx'
 
 const INTERVAL_SPEED = 10
 
 const Timer = () => {
-  const [remainingTime, setRemainingTime] = useState<number>(30_000)
+  const INITIAL_TIME = 30_000
+  const [remainingTime, setRemainingTime] = useState<number>(INITIAL_TIME)
   const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false)
   const [seconds, decimals] = splitSeconds(remainingTime)
+  const percentageElapsed = 100 - (remainingTime * 100) / INITIAL_TIME
 
   const pauseOrPlay = () => {
+    if (remainingTime === 0) {
+      setRemainingTime(INITIAL_TIME)
+    }
     setIsTimerRunning((isCurrentlyRunning) => !isCurrentlyRunning)
   }
 
   useInterval(
     () => {
-      setRemainingTime((timeRemaining) => timeRemaining - INTERVAL_SPEED)
+      if (remainingTime > 0) {
+        setRemainingTime((timeRemaining) => timeRemaining - INTERVAL_SPEED)
+      } else {
+        setIsTimerRunning(false)
+      }
     },
     isTimerRunning ? INTERVAL_SPEED : null
   )
 
   return (
     <div className={styles.timer}>
-      <div
-        className={clsx(styles.time, isTimerRunning && styles.running)}
-        onClick={pauseOrPlay}
-      >
-        <span className={styles.seconds}>{seconds}</span>
-        <span className={styles.point}>.</span>
-        <span className={styles.decimals}>{decimals}</span>
+      <div className={styles.time} onClick={pauseOrPlay}>
+        <div
+          className={styles.progress}
+          style={{ width: `${percentageElapsed}%` }}
+        />
+        <div className={styles.numberWrapper}>
+          <span className={styles.seconds}>{seconds}</span>
+          <span className={styles.point}>.</span>
+          <span className={styles.decimals}>{decimals}</span>
+        </div>
       </div>
     </div>
   )
