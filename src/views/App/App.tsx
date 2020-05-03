77fabae8f03db5792f373442c56b066e08d39bb6
Fernando <fernando@velocity.black>
2020-05-03 19:55:18 +0300

    Fix height for mobile devices

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './App.module.scss'
 import Timer from '../../components/Timer/Timer'
 
-const App = () => (
-  <div className={styles.app}>
-    <Timer />
-  </div>
-)
+const App = () => {
+  const setAppHeight = () => {
+    const element = document.documentElement
+    element.style.setProperty('--app-height', `${window.innerHeight}px`)
+  }
+
+  useEffect(() => {
+    setAppHeight()
+    window.addEventListener('resize', setAppHeight)
+
+    return () => {
+      window.removeEventListener('resize', setAppHeight)
+    }
+  }, [])
+
+  return (
+    <div>
+      <Timer />
+    </div>
+  )
+}
 
 export default App
