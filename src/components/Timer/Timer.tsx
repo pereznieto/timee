4e60de17f71a692f1b87dc0544b2677edcd91472
Fernando <fernando@velocity.black>
2020-05-05 19:47:25 +0300

    Clean up Timer and sounds

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -2,27 +2,47 @@ import React, { useState, useEffect, useRef } from 'react'
 import styles from './Timer.module.scss'
 import { splitSeconds, padZero } from '../../utils/parse'
 import useInterval from '../../utils/useInterval'
-import { playSound } from '../../utils/playSound'
-import ding from '../../assets/ding.mp3'
-import cling from '../../assets/cling.wav'
+import dingSource from '../../assets/ding.mp3'
+import clingSource from '../../assets/cling.wav'
 import clsx from 'clsx'
 
 const TICK_SPEED = 10
 
 const Timer = () => {
   const DEFAULT_DURATION = 30_000
-  const RUNNING_OUT_MARK = 4_000
+  const RUNNING_OUT_MARK = 3_000
   const [duration, setDuration] = useState<number>(DEFAULT_DURATION)
   const [timeLeft, setTimeLeft] = useState<number>(duration)
-  const [runningStartTime, setRunningStartTime] = useState<number | false>(
-    false
-  )
-  const [wasSpacePressed, setWasSpacePressed] = useState<boolean>(false)
+  const [startTime, setStartTime] = useState<number | false>(false)
+  const [wasKeyPressed, setWasKeyPressed] = useState<boolean>(false)
   const seconds = splitSeconds(timeLeft)
   const percentageElapsed = 100 - (timeLeft * 100) / duration
   const shouldShowProgress = timeLeft !== duration
   const isRunningOut = timeLeft < RUNNING_OUT_MARK
-  const dingAudio = useRef(new Audio(ding))
+  const ding = useRef(new Audio(dingSource))
+  const cling = useRef(new Audio(clingSource))
+
+  useEffect(() => {
+    const upHandler = ({ key }: KeyboardEvent): void => {
+      if (key === ' ' || key === 'Enter') {
+        setWasKeyPressed(true)
+      }
+    }
+
+    window.addEventListener('keyup', upHandler)
+
+    return () => {
+      window.removeEventListener('keyup', upHandler)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (wasKeyPressed) {
+      pauseOrPlay()
+      setWasKeyPressed(false)
+    }
+    // eslint-disable-next-line
+  }, [wasKeyPressed])
 
   useEffect(() => {
     if (timeLeft !== duration) {
@@ -32,61 +52,41 @@ const Timer = () => {
   }, [duration])
 
   useEffect(() => {
-    window.addEventListener('keyup', upHandler)
-    return () => {
-      window.removeEventListener('keyup', upHandler)
-    }
-  }, [])
-
-  const upHandler = ({ key }: KeyboardEvent): void => {
-    if (key === ' ') {
-      setWasSpacePressed(true)
-    }
-  }
-
-  useEffect(() => {
-    if (wasSpacePressed) {
-      pauseOrPlay()
-      setWasSpacePressed(false)
+    if (startTime && [3, 2, 1].indexOf(seconds) > -1) {
+      cling.current.play().catch(alert)
     }
     // eslint-disable-next-line
-  }, [wasSpacePressed])
+  }, [seconds])
 
   useInterval(
     () => {
-      const delta = Date.now() - (runningStartTime as number)
-      const timeElapsed = Math.floor(delta / TICK_SPEED) * TICK_SPEED
+      const timeDelta = Date.now() - (startTime as number)
+      const timeElapsed = Math.floor(timeDelta / TICK_SPEED) * TICK_SPEED
       if (timeLeft > 0) {
         setTimeLeft(duration - timeElapsed)
-        if ([3_000, 2_000, 1_000].indexOf(timeLeft) > -1) {
-          playSound(cling)
-        }
       } else {
-        setRunningStartTime(false)
-        dingAudio.current.play().catch(alert)
+        setStartTime(false)
+        ding.current.play().catch(alert)
       }
     },
-    runningStartTime ? TICK_SPEED : null
+    startTime ? TICK_SPEED : null
   )
 
   const pauseOrPlay = (): void => {
-    // dingAudio.current.play()
     if (timeLeft === 0) {
       setTimeLeft(duration)
-      setRunningStartTime(Date.now())
+    }
+    if (timeLeft === 0 || timeLeft === duration) {
+      setStartTime(Date.now())
     } else {
-      if (timeLeft === duration) {
-        setRunningStartTime(Date.now())
-      } else {
-        setRunningStartTime((isCurrentlyRunning) =>
-          isCurrentlyRunning ? false : Date.now() - (duration - timeLeft)
-        )
-      }
+      setStartTime((isCurrentlyRunning) =>
+        isCurrentlyRunning ? false : Date.now() - (duration - timeLeft)
+      )
     }
   }
 
   const getHelperAction = (): string => {
-    if (runningStartTime) {
+    if (startTime) {
       return 'pause'
     }
 
@@ -109,7 +109,7 @@ const Timer = () => {
             className={clsx(
               styles.progress,
               isRunningOut && styles.runningOut,
-              !runningStartTime && styles.notRunning
+              !startTime && styles.notRunning
             )}
             style={{ width: `${percentageElapsed}%` }}
           />
@@ -134,7 +134,7 @@ const Timer = () => {
             setDuration(Number(value) * 1000)
           }}
           onClick={(event) => {
-            if (!runningStartTime) {
+            if (!startTime) {
               event.stopPropagation()
             }
           }}
--- src/utils/playSound.ts
+++
@@ -1,7 +0,0 @@
-export const playSound = (fileSource: string, volume: number = 1): void => {
-  const audio: HTMLAudioElement = new Audio(fileSource)
-  if (audio) {
-    audio.volume = volume
-    audio.play().catch(console.error)
-  }
-}
